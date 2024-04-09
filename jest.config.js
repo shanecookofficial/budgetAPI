d726a6129e02543e135148159f737635b58b8989
shanecookofficial <shanecookofficial@gmail.com>
2024-04-08 22:30:05 -0500

    Updated jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     transformIgnorePatterns: [
-      '/node_modules/(?!bson)/' // Ignore all node_modules except the bson package
-    ]
-  };
-  
+        '/node_modules/(?!bson)/' // Ignore all node_modules except the bson package
+    ],
+    extensionsToTreatAsEsm: ['.js'], // Treat .js files as ECMAScript modules
+    globals: {
+        'ts-jest': {
+            useESM: true, // Use ECMAScript modules
+        },
+    },
+};
